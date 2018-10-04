a7e1c3140e857beb6f8fce2af27ccbd06085f516
Powerrice1 <thenguyenshow@gmail.com>
2018-10-03 21:27:24 -0500

    This is a test from Nathan.

--- ShapeShifter/Assets/Scripts/PlayerController.cs
+++ ShapeShifter/Assets/Scripts/PlayerController.cs
@@ -2,60 +2,95 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
 public class PlayerController : MonoBehaviour {
-    public float speed = 10.0f;
-    public float jumpForce;
+	public float speed;
+	public float jumpForce;
 
-    private Rigidbody2D rb;
+	private Rigidbody2D rb;
 
-    private bool facingRight = true;
+	private bool facingRight = true;
 
-    private bool isGrounded;
-    public Transform groundCheck;
-    public float checkRadius;
-    public LayerMask whatIsGround;
+	private bool isGrounded;
+	public Transform groundCheck;
+	public float checkRadius;
+	public LayerMask whatIsGround;
 
-    private int extraJumps;
-    public int extraJumpsValue;
+	private int extraJumps;
+	public int extraJumpsValue;
+
+	public Animator animator;
+	public Animator animator2;
+
+	public GameObject baseF, knight;
+	int playerSelect;
 
 	// Use this for initialization
 	void Start () {
-        extraJumps = extraJumpsValue;
-        rb = GetComponent<Rigidbody2D>();
+		playerSelect = 1;
+		baseF = GameObject.Find ("baseF");
+		knight = GameObject.Find ("knight_p");
+
+		extraJumps = extraJumpsValue;
+		rb = GetComponent<Rigidbody2D>();
 	}
 
-    // Update is called once per frame
-    private void Update() {
-        if(isGrounded == true) {
-            extraJumps = extraJumpsValue;
-        }
-        if (Input.GetButtonDown("Jump") && extraJumps > 0) {
-            rb.velocity = Vector2.up * jumpForce;
-            extraJumps--;
-        } else if(Input.GetButtonDown("Jump") && extraJumps == 0 && isGrounded == true) {
-            rb.velocity = Vector2.up * jumpForce;
-        }
-    }
+	// Update is called once per frame
+	private void Update() {
+		if(isGrounded == true) {
+			extraJumps = extraJumpsValue;
+		}
+		if (Input.GetButtonDown("Jump") && extraJumps > 0) {
+			rb.velocity = Vector2.up * jumpForce;
+			extraJumps--;
+		} else if(Input.GetButtonDown("Jump") && extraJumps == 0 && isGrounded == true) {
+			rb.velocity = Vector2.up * jumpForce;
+		}
+
+		if (Input.GetButtonDown ("Change")) {
+			if (playerSelect == 1)
+				playerSelect = 2;
+			else
+				playerSelect = 1;
+		}
+
+		if (playerSelect == 1) {
+			baseF.SetActive (true);
+			knight.SetActive (false);
+			speed = 12.0f;
+			jumpForce = 10.0f;
+		} else {
+			baseF.SetActive (false);
+			knight.SetActive (true);
+			speed = 5.5f;
+			jumpForce = 0.0f;
+		}
+	}
 
 
-    void FixedUpdate () {
-        // left & right movement
-        float xTranslation = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(xTranslation * speed, rb.velocity.y);
+	void FixedUpdate () {
+		// left & right movement
+		float xTranslation = Input.GetAxis("Horizontal");
+		animator.SetFloat ("Speed", Mathf.Abs(xTranslation));
+		animator2.SetFloat ("Speed", Mathf.Abs(xTranslation));
+		rb.velocity = new Vector2(xTranslation * speed, rb.velocity.y);
 
-        // flips sprite if moving the other direction
-        if ((facingRight == true && xTranslation < 0) || (facingRight == false && xTranslation > 0))
-            Flip();
+		// flips sprite if moving the other direction
+		if ((facingRight == true && xTranslation < 0) || (facingRight == false && xTranslation > 0))
+			Flip();
 
-        // checks if player is touching the ground
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
-    }
+		// checks if player is touching the ground
+		isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+	}
 
-    void Flip()
-    {
-        facingRight = !facingRight;
-        Vector3 scaler = transform.localScale;
-        scaler.x *= -1;
-        transform.localScale = scaler;
-    }
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector3 scaler = transform.localScale;
+		scaler.x *= -1;
+		transform.localScale = scaler;
+	}
 }
