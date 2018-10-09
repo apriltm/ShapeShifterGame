d2fd1d19f3587d775927c5c064735154c6f1a92a
kmdauwa <kaifdauwa@gmail.com>
2018-10-09 15:51:14 -0500

    Added Healthbar
    HealthBar added.

--- ShapeShifter/Assets/Scripts/Player'sScripts/PlayerHealth.cs
+++ ShapeShifter/Assets/Scripts/Player'sScripts/PlayerHealth.cs
@@ -1,19 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour {
 
-	public int maxHealth;
-	public int currentHealth;
+	public float maxHealth;
+	public float currentHealth;
 	private PlayerController player;
 
-	
+    public Image currentHPBar;
+    public Text HPText;
+
 	// Use this for initialization
 	void Start () {
 		
 		currentHealth = maxHealth;
 		player = gameObject.GetComponent<PlayerController> ();
+        UpdateHealthBar();
 	}
 
 	// Update is called once per frame
@@ -28,22 +32,44 @@ public class PlayerHealth : MonoBehaviour {
 
 	}
 
+    
+
 	void FixedUpdate() {
 		
 	}
 
+    public void UpdateHealthBar()
+    {
+        float ratio = currentHealth / maxHealth;
+        currentHPBar.rectTransform.localScale = new Vector3(ratio, 1, 1);
+        HPText.text = currentHealth.ToString();
+
+        if (currentHealth <= 60)
+        {
+            currentHPBar.color = new Color32(188, 141, 88, 255);
+        }
+
+        if (currentHealth <= 30)
+        {
+            currentHPBar.color = new Color32(188, 44, 28, 255);
+        }
+    }
+
 	public void TakeDamage(int dam){
 
-		currentHealth -= dam;
-		Debug.Log (currentHealth);
+		
 
 		if (currentHealth > 0) {
-			player.animator.SetTrigger ("Damaged");
+            currentHealth -= dam;
+            Debug.Log(currentHealth);
+            player.animator.SetTrigger ("Damaged");
 			Audio.PlaySound ("PlayerHurt");
 			//gameObject.GetComponent<Animation> ().Play ("Hurt");
 
 
-		}		
+		}
+
+        UpdateHealthBar();
 	}
 	 
 
