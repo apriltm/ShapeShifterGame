2cd321e591f957a67b9a9b57b02abee0395ed05b
Powerrice1 <thenguyenshow@gmail.com>
2018-10-17 00:58:53 -0500

    dies after staying in water for 10.5 sec(can change)

---
+++ ShapeShifter/Assets/Scripts/Player'sScripts/PlayerDrowning.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDrowning : MonoBehaviour {
+
+	public float drownTime;
+	private float drownTimer;
+	private PlayerHealth player;
+
+	// Use this for initialization
+	void Start () {
+		player = gameObject.GetComponent<PlayerHealth> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay2D(Collider2D collision){
+		if (collision.tag == "Water") {
+			drownTimer += Time.deltaTime;
+			Debug.Log ("underwater");
+		} else {
+			drownTimer = 0;
+		}
+
+		if (drownTimer > drownTime) {
+			player.currentHealth = 0;
+		}
+	}
+}
