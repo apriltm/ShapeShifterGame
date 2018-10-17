df43671d8f22a882c130a4690a6bf112f1b96f24
apriltm <april_mcrae@hotmail.com>
2018-10-17 00:10:10 -0500

    Adding new scene as well as other assets, music, scripts, tiles.

---
+++ ShapeShifter/Assets/Water.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Water : MonoBehaviour {
+
+    [SerializeField]
+    private Rigidbody2D rb;
+    private PlayerController player;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+ 
+        rb.drag = 30f;
+        player = gameObject.GetComponent<PlayerController>();
+        player.Invoke("Dies", 5f);
+
+
+    }
+
+    private void OnTriggerExit2D(Collider2D player)
+    {
+        if(player.CompareTag("Player")){
+            rb.gravityScale = 3.55f;
+        }
+    }
+
+}
