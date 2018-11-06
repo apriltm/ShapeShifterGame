31654da747156113c6d5bdec0d3805a04598b4c9
Powerrice1 <thenguyenshow@gmail.com>
2018-11-06 03:50:46 -0600

    working with lights again

---
+++ ShapeShifter/Assets/MaterialController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialController : MonoBehaviour {
+
+    public SpriteRenderer main, knight, mage, archer, NYAN, arrow, shield;
+    public Material Default;
+    public Material Dark;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void OnTriggerStay2D (Collider2D col) {
+
+        main.material = Dark;
+        knight.material = Dark;
+        mage.material = Dark;
+        archer.material = Dark;
+        NYAN.material = Dark;
+        arrow.material = Dark;
+        shield.material = Dark;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+        main.material = Default;
+        knight.material = Default;
+        mage.material = Default;
+        archer.material = Default;
+        NYAN.material = Default;
+        arrow.material = Default;
+        shield.material = Default;
+    }
+
+
+}
---
+++ ShapeShifter/Assets/NYAN.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NYAN : MonoBehaviour {
+
+    public GameObject NYANCAT;
+    public GameObject SS;
+
+	// Use this for initialization
+	void Start () {
+        NYANCAT.SetActive(false);
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if( collision.tag == "darkness")
+        {
+            Audio.PlaySound("Shift");
+            Instantiate(SS, NYANCAT.transform.position, transform.rotation = Quaternion.identity);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.tag == "darkness")
+        {
+            NYANCAT.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "darkness")
+        {
+            NYANCAT.SetActive(false);
+            Audio.PlaySound("Shift");
+            Instantiate(SS, NYANCAT.transform.position, transform.rotation = Quaternion.identity);
+        }
+    }
+}
